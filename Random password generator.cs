409dd9dd76c27dfc4262610278c925d0aa27e32e
misakiyuki18 <106395754+misakiyuki18@users.noreply.github.com>
2022-09-16 01:12:39 +0200

    Add files via upload

---
+++ Random password generator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp5
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "0123456789";
+            var x = new Random();
+            int lenght = 12;
+            
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int y = x.Next(15);
+                if (y < 5)
+                {
+                    for (int m = 0; i < lenght; i++)
+                    {
+                        int a = x.Next(9);
+                        Console.Write(lower[a]);
+                        break;
+                    }
+                }
+                if (y >= 5 && y < 10)
+                {
+                    for (int n = 0; i < lenght; i++)
+                    {
+                        int a = x.Next(9);
+                        Console.Write(upper[a]);
+                        break;
+                    }
+                }
+                if (y >= 10)
+                {
+                    for (int o = 0; i < lenght; i++)
+                    {
+                        int a = x.Next(9);
+                        Console.Write(numbers[a]);
+                        break;
+                    }
+                }
+            }
+
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+   
+}
